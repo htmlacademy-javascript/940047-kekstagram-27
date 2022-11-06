c34475259d76358417d65d53069e0b8f521eb6db
Anastasia Kulaeva <kulaeva.anastasia1995@yandex.ru>
2022-11-06 15:36:29 +0300

    Удаляем комментарии в коде

--- js/picture.js
+++ js/picture.js
@@ -1,20 +1,20 @@
 import {createPhotos} from './data.js';
 
-const similarListElement = document.querySelector('.pictures'); //Контейнер для изображений от других пользователей
-const similarPhotoElement = document.querySelector('#picture') //Шаблон изображения случайного пользователя, который копируем
+const similarListElement = document.querySelector('.pictures');
+const similarPhotoElement = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const similarPhoto = createPhotos(); // то что импортировали
+const similarPhoto = createPhotos();
 
-const similarListFragment = document.createDocumentFragment(); // вставляем всё через DocumentFragmen
+const similarListFragment = document.createDocumentFragment();
 
 similarPhoto.forEach(({url,comments, likes}) => {
-  const photoElement = similarPhotoElement.cloneNode(true); //клонируем шаблон
+  const photoElement = similarPhotoElement.cloneNode(true);
   photoElement.querySelector('.picture__img').src = url;
   photoElement.querySelector('.picture__comments').textContent = comments;
   photoElement.querySelector('.picture__likes').textContent = likes;
-  similarListFragment.appendChild(photoElement); //отрисовка
+  similarListFragment.appendChild(photoElement);
 });
 
 similarListElement.appendChild(similarListFragment);
